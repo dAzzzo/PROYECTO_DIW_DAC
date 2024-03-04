df871c53086316c6053e0d67034fcf77a71228a7
Daniel <dandcom0601@g.educaand.es>
2024-03-04 14:47:59 +0100

    implementacion del dark-mode, mejora de las RRSS y organizacion de ficheros

--- js/accesibilidad.js
+++ js/accesibilidad.js
@@ -1,18 +1,11 @@
 function cambiarColor() {
-  //Obtener el boton toggle
   let toggleButton = document.getElementById("button");
 
-  toggleButton.addEventListener("", function () {
-    let body = document.querySelector("body");
-
-    if (body.classList.contains("dark-mode")) {
-      body.classList.remove("dark-mode");
-
-      toggleButton.innerHTML = "modo oscuro";
-    } else {
-      body.classList.add("dark-mode");
-
-      toggleButton.innerHTML = "modo claro";
-    }
-  });
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+    toggleButton.innerHTML = "☼";
+  } else {
+    document.body.classList.add("dark-mode");
+    toggleButton.innerHTML = "☾";
+  }
 }
